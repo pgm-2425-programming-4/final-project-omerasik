f68bca62af0e22c38f8a1f3e6d6c30dfb246cf6d
omerasik <omerasik60@gmail.com>
2025-05-27 10:42:38 +0200

    update

--- client/my-app/src/constants/constants.js
+++ client/my-app/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://jammin-api-cppe.onrender.com";
+export const API_URL = "https://jammin-api-cppe.onrender.com/api";
 export const API_TOKEN =
   "aeca31a06c36cfc27be331c07f4b23c3396a8bf68e6570789c6149a56b7cb0cac28b4428011ffc8b0e662972d90b0cca484daa28b58f97ba7f2bbcf4bbec0fa675221bdeb542ce3828b119f33b1953a69ac15486b317bfc1e3ceeb081e283d77ec320bf540ec29dd3f87ec5caa0098d6eebc32d633bce3d36edd3125778326d0";
 export const PAGE_SIZE_OPTIONS = [5, 10, 25, 50];
