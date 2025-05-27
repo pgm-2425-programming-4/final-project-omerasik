eebdfa2a334888fe1e3ef9b73aaac007cfb30183
omerasik <omerasik60@gmail.com>
2025-05-27 10:47:41 +0200

    update

--- client/my-app/src/constants/constants.js
+++ client/my-app/src/constants/constants.js
@@ -1,11 +1,11 @@
-export const API_URL = "https://jammin-api-cppe.onrender.com/api";
-export const API_TOKEN =
-  "aeca31a06c36cfc27be331c07f4b23c3396a8bf68e6570789c6149a56b7cb0cac28b4428011ffc8b0e662972d90b0cca484daa28b58f97ba7f2bbcf4bbec0fa675221bdeb542ce3828b119f33b1953a69ac15486b317bfc1e3ceeb081e283d77ec320bf540ec29dd3f87ec5caa0098d6eebc32d633bce3d36edd3125778326d0";
+const isProd = import.meta.env.PROD;
+
+export const API_URL = isProd
+  ? "https://jammin-api-cppe.onrender.com/api"
+  : "http://localhost:1337/api";
+
+export const API_TOKEN = isProd
+  ? "aeca31a06c36cfc27be331c07f4b23c3396a8bf68e6570789c6149a56b7cb0cac28b4428011ffc8b0e662972d90b0cca484daa28b58f97ba7f2bbcf4bbec0fa675221bdeb542ce3828b119f33b1953a69ac15486b317bfc1e3ceeb081e283d77ec320bf540ec29dd3f87ec5caa0098d6eebc32d633bce3d36edd3125778326d0"
+  : "135a3a0969e589ca53b60d38100a51d95c835a65fdf0ea05009e24586fd5f9c803be1cfc0496eef1c6a14fc2440ab3eb1f22b9fb386e33ff1fc5a8fcda76d81ee224d1a4b9b5579033fe85a481fde9174fd466e4394923c6b5c5e7877eb018594e38fc0538e1b049b2c786eb09a4d5f0ac349abb52e794065dd864ef6082b4c2";
+
 export const PAGE_SIZE_OPTIONS = [5, 10, 25, 50];
-
-// local development constants
-
-// export const API_URL = "http://localhost:1337/api";
-// export const API_TOKEN =
-//   "135a3a0969e589ca53b60d38100a51d95c835a65fdf0ea05009e24586fd5f9c803be1cfc0496eef1c6a14fc2440ab3eb1f22b9fb386e33ff1fc5a8fcda76d81ee224d1a4b9b5579033fe85a481fde9174fd466e4394923c6b5c5e7877eb018594e38fc0538e1b049b2c786eb09a4d5f0ac349abb52e794065dd864ef6082b4c2";
-// export const PAGE_SIZE_OPTIONS = [5, 10, 25, 50]; // 
