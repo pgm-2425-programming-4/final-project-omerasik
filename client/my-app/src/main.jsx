0e1155d33c765945c3e391b520126f1e829ddc86
omerasik <omerasik60@gmail.com>
2025-06-09 19:07:52 +0200

    setup  basic index.html and main.jsx

--- client/my-app/src/main.jsx
+++ client/my-app/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from "./components/app/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from '../routeTree'
+import '../index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from '../components/app/App'
+
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
